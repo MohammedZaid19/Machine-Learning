b237207decc5a088c7e53acb6c12a0ed1e762f6d
Zaid <mkhalidkachhawa@gmail.com>
2026-06-18 13:24:09 +0530

    Again changes

--- matplotlib_codes.py
+++ matplotlib_codes.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
-xpoints = np.array([1,8])
-ypoints = np.array([3,10])
-plt.plot(xpoints,ypoints)
+# xpoints = np.array([1,8])
+# ypoints = np.array([3,10])
+y = np.array([35,25,25,15])
+plt.pie(y)
 plt.show()
