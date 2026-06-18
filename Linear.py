55952e2fb85f14ef4c767994819f9b70ad9fbec0
Zaid <mkhalidkachhawa@gmail.com>
2026-06-18 12:57:00 +0530

    Pandas filed added

--- Linear.py
+++ Linear.py
@@ -1,5 +1,5 @@
 # step 1 Import Libraries
-import pandas as pd
+import pandas  as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error , r2_score
---
+++ panda_code.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df = pd.read_csv('D:/Downloads/archive/ai_student_impact_dataset (1).csv')
+print(df.to_string())
