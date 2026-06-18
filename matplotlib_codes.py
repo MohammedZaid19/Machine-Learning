4f85e123ca8a8eeae7a9b9c4b95368f15e959c73
Zaid <mkhalidkachhawa@gmail.com>
2026-06-18 18:02:00 +0530

    3 codes have been added in the matplotlib file

--- matplotlib_codes.py
+++ matplotlib_codes.py
@@ -1,7 +1,31 @@
 import matplotlib.pyplot as plt
-import numpy as np
+# import numpy as np
 # xpoints = np.array([1,8])
 # ypoints = np.array([3,10])
-y = np.array([35,25,25,15])
-plt.pie(y)
+# y = np.array([35,25,25,15])
+# plt.pie(y)
+# plt.show()
+# Scatterchart
+# x = [1,2,3,4,5]
+# y = [10,15,20,25,30]
+# plt.scatter(x,y)
+# plt.title("Scatter Chart")
+# plt.xlabel("X Values")
+# plt.ylabel("Y Values")
+# plt.show()
+# bar Chart
+# students = ["A","B","C","D","E"]
+# marks = [80,70,90,60,65]
+# plt.bar(students,marks)
+# plt.title("bar Chart")
+# plt.xlabel("Students")
+# plt.ylabel("Marks")
+# plt.show()
+# Line chart
+days = [1,2,3,4,5]
+sales = [100,150,120,180,200]
+plt.plot(days,sales,marker='o')
+plt.title("Line Chart")
+plt.xlabel("Days")
+plt.ylabel("Sales")
 plt.show()
