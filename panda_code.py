582fd375a333ca3156abf7854c72b9c95596391b
Zaid <mkhalidkachhawa@gmail.com>
2026-06-18 13:08:21 +0530

    changes done in panda

--- panda_code.py
+++ panda_code.py
@@ -1,3 +1,13 @@
-import pandas as pd
-df = pd.read_csv('D:/Downloads/archive/ai_student_impact_dataset (1).csv')
-print(df.to_string())
+
+# import pandas as pd
+# df = pd.read_csv('D:/Downloads/archive/ai_student_impact_dataset (1).csv')
+# print(df.to_string())
+# print(df.head(15))
+#
+# # import pandas as pd
+# mydataset = {
+#     "Cars" : ["BMW","Benz"],
+#     "passings" : [3,7]
+# }
+# myvar = pandas.DataFrame(mydataset)
+# print(myvar)
